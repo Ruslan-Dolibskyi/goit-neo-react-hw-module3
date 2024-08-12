ee494edecf8aa028d71f75668faba2b582360f3e
Ruslan Dolibskyi <dolibskiyruslan.gmail.com>
2024-08-12 07:09:53 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,17 +5,26 @@ import SearchBox from '../SearchBox/SearchBox';
 import styles from './App.module.css';
 import { nanoid } from 'nanoid';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+  });
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    if (contacts.length > 0) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    } else {
+      localStorage.removeItem('contacts');
+    }
   }, [contacts]);
 
   const addContact = (name, number) => {
